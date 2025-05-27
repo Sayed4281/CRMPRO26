e868cc09d808323c6f5bd321b67d8603f2317e8d
MOHAMMED AFLAH <mohdaflah77@gmail.com>
2025-05-27 20:29:53 +0000

    Update analysis_options.yaml to ignore additional analyzer errors and modify AddEnquiryScreen constructor for improved syntax

--- lib/screens/add_enquiry.dart
+++ lib/screens/add_enquiry.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AddEnquiryScreen extends StatelessWidget {
-  const AddEnquiryScreen({Key? key}) : super(key: key);
+  const AddEnquiryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
